a17d657bb3105b003ecb6d29a75bb8d5334b38ad
irelandpenguin <irelandspider@gmail.com>
2021-01-17 19:47:45 +0800

    com.irelandpenguin_06

---
+++ 01_HelloWorld/src/com/irelandpenguin_06/SingAndDance.java
@@ -0,0 +1,14 @@
+package com.irelandpenguin_06;
+
+/**
+ * ½Ó¿Ú
+ * 
+ * @author irelandpenguin
+ * 
+ * 
+ */
+public interface SingAndDance {
+	public abstract void sing();
+
+	public abstract void dance();
+}
